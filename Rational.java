c4a6126631b7952cbbe8d9347f389b50a8fd99b6
franz-eric.sill <franz-eric.sill@th-bingen.de>
2022-06-04 11:36:20 +0200

    Added basic methods to Rational, changed to BigInteger

--- Rational.java
+++ Rational.java
@@ -1,7 +1,87 @@
+import java.math.BigInteger;
+
 public class Rational{
-    int nom, denom;
-    Rational(int n, int d){
-        nom = n;
-        denom = d;
+    final BigInteger nom, denom;
+    Rational(BigInteger nom, BigInteger denom){
+        if (denom.compareTo(BigInteger.ZERO) == 0)
+            throw new IllegalArgumentException("Zero in denominator");
+        BigInteger m = nom;
+        BigInteger n = denom;
+        BigInteger r = m.remainder(n);
+        while (r.compareTo(BigInteger.ZERO) != 0) {
+            m = n;
+            n = r;
+            r = m.remainder(n);
+        }
+        this.nom = nom.divide(n);
+        this.denom = denom.divide(n);
+        assert (this.denom).compareTo(BigInteger.ZERO) >= 0:"Denominator negative";
+        assert (this.denom).compareTo(BigInteger.ZERO) != 0:"Denominator zero";
+        assert (this.nom).gcd(denom).compareTo(BigInteger.ONE) == 0:"Rational not shortened correctly";
+    }
+    Rational(BigInteger nom){
+        this(nom, BigInteger.ONE);
+    }
+    BigInteger getNom(){
+        return nom;
+    }
+    BigInteger getDenom(){
+        return denom;
+    }
+    Rational add(Rational r){
+        return new Rational(nom.multiply(r.getDenom()).add(r.getNom().multiply(denom)), denom.multiply(r.getDenom()));
+    }
+    Rational sub(Rational r){
+        return add(r.negate());
+    }
+    Rational mult(Rational r){
+        return new Rational(nom.multiply(r.getNom()), denom.multiply(r.getDenom()));
+    }
+    Rational div(Rational r){
+        return mult(r.reciprocal());
+    }
+    Rational pow(int n){
+        Rational result = this;
+        if (n == 0){
+            return new Rational(BigInteger.ONE, BigInteger.ONE);
+        }
+        if (n < 0) {
+            result = result.reciprocal();
+            n = -n;
+        }
+        for (int i = 1; i < n; i++) {
+            result = result.mult(this);
+        }
+        return result;
+    }
+    Rational reciprocal(){
+        return new Rational(denom, nom);
+    }
+    Rational negate(){
+        return new Rational(nom.negate(), denom);
+    }
+    int compareTo(Rational r){
+        return (nom.multiply(r.getDenom())).compareTo(r.getNom().multiply(denom));
+    }
+    boolean gr(Rational r){
+        return compareTo(r) == 1;
+    }
+    boolean lo(Rational r){
+        return compareTo(r) == -1;
+    }
+    boolean ge(Rational r){
+        return compareTo(r) > -1;
+    }
+    boolean le(Rational r){
+        return compareTo(r) < 1;
+    }
+    boolean ne(Rational r){
+        return compareTo(r) != 0;
+    }
+    public boolean equals(Rational r){
+        return compareTo(r) == 0;
+    }
+    public String toString(){
+        return nom.toString() + "/" + denom.toString();
     }
 }

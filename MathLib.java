d77214a92a2a46dbc4de99903682d2b70930371f
franz-eric.sill <franz-eric.sill@th-bingen.de>
2022-06-04 17:35:15 +0200

    Introducing an own MathLib

---
+++ MathLib.java
@@ -0,0 +1,21 @@
+import java.math.BigDecimal;
+
+class MathLib{
+    public static final BigDecimal PI = new BigDecimal("3.14159265358979323846264338327950288419716939937510582097494");
+    public static RealNumber max(RealNumber d, RealNumber... bigD){
+        RealNumber max = d;
+        for (RealNumber i : bigD) {
+            if (i.compareTo(max) > 0)
+                max = i;
+        }
+        return max;
+    }
+    public static RealNumber min(RealNumber d, RealNumber... bigD){
+        RealNumber min = d;
+        for (RealNumber i : bigD) {
+            if (i.compareTo(min) < 0)
+                min = i;
+        }
+        return min;
+    }
+}
